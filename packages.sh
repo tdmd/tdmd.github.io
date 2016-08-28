f8123eed9a1b37ff5b81114cd9b7c012406d5c2d
tdmd <tdmd@outlook.com>
2016-08-28 17:19:01 -0500

    fixed battery lockscreens
    fixed battery lockscreens

--- packages.sh
+++ packages.sh
@@ -1,6 +1,3 @@
-dpkg-deb -Zgzip -b com.tdmd.lockglyph-ironman1
-dpkg-deb -Zgzip -b com.tdmd.clockCalendarBanner1
-dpkg-deb -Zgzip -b com.tdmd.clockCalendarBanner2
 dpkg-deb -Zgzip -b com.tdmd.IS2-zeldaLS
 dpkg-deb -Zgzip -b com.tdmd.IS2-marioLS
 dpkg-deb -Zgzip -b com.tdmd.IS2-battery-circles
