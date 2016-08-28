da49b361a884fd6fa60f5a336478bea383c913cf
tdmd <tdmd@outlook.com>
2016-08-28 16:54:43 -0500

    fixed changes
    fixed changes

--- packages.sh
+++ packages.sh
@@ -1,2 +1,6 @@
+dpkg-deb -Zgzip -b com.tdmd.lockglyph-ironman1
 dpkg-deb -Zgzip -b com.tdmd.clockCalendarBanner1
 dpkg-deb -Zgzip -b com.tdmd.clockCalendarBanner2
+dpkg-deb -Zgzip -b com.tdmd.IS2-zeldaLS
+dpkg-deb -Zgzip -b com.tdmd.IS2-marioLS
+dpkg-deb -Zgzip -b com.tdmd.IS2-battery-circles
