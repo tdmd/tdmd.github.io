bec0e1571c1086611975f901d6911ac70699d61a
tdmd <tdmd@outlook.com>
2016-08-28 23:12:26 -0500

    tdMD LS13
    tdMD LS13

--- packages.sh
+++ packages.sh
@@ -1,3 +1,1 @@
-dpkg-deb -Zgzip -b com.tdmd.clockCalendarBanner2-LS
-dpkg-deb -Zgzip -b com.tdmd.clockCalendarBanner1-LS
-dpkg-deb -Zgzip -b com.tdmd.clockCalendarBanner2
+dpkg-deb -Zgzip -b com.tdmd.LS13
