10f381365d6c4d562c538934b78978b67d9723e3
tdmd <tdmd@outlook.com>
2016-08-28 21:15:16 -0500

    fixed 2nd banner, added banners for LockHTML
    fixed 2nd banner, added banners for LockHTML

--- packages.sh
+++ packages.sh
@@ -1,2 +1,3 @@
-dpkg-deb -Zgzip -b com.tdmd.clockCalendarBanner1
+dpkg-deb -Zgzip -b com.tdmd.clockCalendarBanner2-LS
+dpkg-deb -Zgzip -b com.tdmd.clockCalendarBanner1-LS
 dpkg-deb -Zgzip -b com.tdmd.clockCalendarBanner2
