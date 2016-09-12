096980eef8e8fe19b8831444d45680e2548cccb5
tdmd <tdmd@outlook.com>
2016-09-12 00:46:52 -0500

    fixed date for clock banners
    fixed date for clock banners

--- packages.sh
+++ packages.sh
@@ -1,2 +1,4 @@
-dpkg-deb -Zgzip -b com.tdmd.iPadXRAY-LS
-dpkg-deb -Zgzip -b com.tdmd.iPhoneXRAY-LS
+dpkg-deb -Zgzip -b com.tdmd.clockCalendarBanner2
+dpkg-deb -Zgzip -b com.tdmd.clockCalendarBanner2-LS
+dpkg-deb -Zgzip -b com.tdmd.clockCalendarBanner1
+dpkg-deb -Zgzip -b com.tdmd.clockCalendarBanner1-LS
