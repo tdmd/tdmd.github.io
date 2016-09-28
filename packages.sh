5071bff7193fabc9e8a2898c1252439c35c2a887
tdmd <tdmd@outlook.com>
2016-09-27 22:05:06 -0500

    space potato salad
    space potato salad

--- packages.sh
+++ packages.sh
@@ -1,4 +1,1 @@
-dpkg-deb -Zgzip -b com.tdmd.clockCalendarBanner2
-dpkg-deb -Zgzip -b com.tdmd.clockCalendarBanner2-LS
-dpkg-deb -Zgzip -b com.tdmd.clockCalendarBanner1
-dpkg-deb -Zgzip -b com.tdmd.clockCalendarBanner1-LS
+dpkg-deb -Zgzip -b com.tdmd.SpacePotatoSalad
